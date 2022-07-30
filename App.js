4f1135078b60e2cf7e7d6335a739baea4ee6b1cd
insub4067 <vb15@kakao.com>
2022-07-30 23:00:08 +0900

    [Add] 삼항연산자를 통한 조건적 return

--- App.js
+++ App.js
@@ -27,16 +27,22 @@ export default function App() {
         {/* 하나의 카드 영역을 나타내는 View */}
         { 
           tip.map((content,i)=>{
-            return (
-            <View style={styles.card} key={i}>
+            return i % 2 == 0 ? (<View style={styles.cardEven} key={i}>
               <Image style={styles.cardImage} source={{uri:content.image}}/>
               <View style={styles.cardText}>
                 <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
                 <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
                 <Text style={styles.cardDate}>{content.date}</Text>
               </View>
-            </View>
-            )
+            </View>) : (<View style={styles.cardOdd} key={i}>
+                <Image style={styles.cardImage} source={{uri:content.image}}/>
+                <View style={styles.cardText}>
+                  <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
+                  <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
+                  <Text style={styles.cardDate}>{content.date}</Text>
+                </View>
+              </View>)
+            
           })
          }
         
@@ -158,6 +164,23 @@ const styles = StyleSheet.create({
     fontSize:10,
     color:"#A6A6A6",
   },
-
+  cardEven:{
+    flex:1,
+    flexDirection:"row",
+    margin:10,
+    backgroundColor:"#FFFED7",
+    borderRadius:20,
+    borderBottomWidth:0.5,
+    borderBottomColor:"#eee",
+    paddingBottom:10
+  },
+  cardOdd:{
+    flex:1,
+    flexDirection:"row",
+    margin:10,
+    borderBottomWidth:0.5,
+    borderBottomColor:"#eee",
+    paddingBottom:10
+  },
 
 });
